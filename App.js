0af195d23810527eb502638baddccabc21977247
kevin vergara <kevinvergara@workmate.cl>
2019-12-06 22:46:48 -0300

    hello world

--- App.js
+++ App.js
@@ -3,28 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      
-      <Text>Step One</Text>
-      <Text>
-        Edit App.js to change this screen and turn it
-        into your app.
-    </Text>
-      <Text>See Your Changes</Text>
-      <Text>
-        Press Cmd + R inside the simulator to reload
-        your app’s code.
-    </Text>
-      <Text>Debug</Text>
-      <Text>
-        Press Cmd + M or Shake your device to open the
-        React Native Debug Menu.
-    </Text>
-      <Text>Learn</Text>
-      <Text>
-        Read the docs to discover what to do next:
-    </Text>
-    </>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, world!</Text>
+    </View>
   );
 }
 
