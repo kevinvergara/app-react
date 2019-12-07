ce84de5e15a5458b2ab5da0573d157fcc34068fd
Daigosk <diego.flores@walmart.com>
2019-12-07 00:04:20 -0300

    create login

--- app/boot/index.js
+++ app/boot/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Images} from "../../assets/index";
+import Image from "react-native-web/dist/exports/Image";
+import TextInput from "react-native-web/src/exports/TextInput";
+import {Button} from "react-native-web";
 
 export default class Container extends Component<{}, State> {
     render() {
         return (
+        <>
+            <View>
+                <Image uri={Images.IconApp} width={scale(60)} height={scale(60)} />
+            </View>
+            <Text> Login to your Account</Text>
+            <TextInput/>
+            <TextInput/>
+            <Button onPress={} title={}/>
+            <Text> -or sign in with-</Text>
             <View style={styles.container}>
                 <Text>Hello, world!</Text>
             </View>
+        </>
     )};
 }
 
---
+++ assets/index.js
@@ -0,0 +1,3 @@
+export const Images = {
+    IconApp: require('./icon.png'),
+};
