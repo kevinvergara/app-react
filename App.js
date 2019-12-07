1bb725d7c1d460302a5c23c857b671d2596a5831
Daigosk <diego.flores@walmart.com>
2019-12-06 23:40:47 -0300

    create container

--- App.js
+++ App.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './app/boot';
 
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello, world!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ app/boot/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Container extends Component<{}, State> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hello, world!</Text>
+            </View>
+    )};
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
