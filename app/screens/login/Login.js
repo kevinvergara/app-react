c46b7f9122099881d8831315602b455495ddf6dd
kevin vergara <kevinvergara@workmate.cl>
2019-12-07 19:17:04 -0300

    design login beta

--- app/screens/login/Login.js
+++ app/screens/login/Login.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Card, CardItem, Text, Body, Title, Left, Right } from 'native-base';
+import { Container, Header, Content, Card, Item, Text, Body, Title, Button, Label, Input } from 'native-base';
 
 export default class Login extends Component {
     render() {
         return (
             <Container>
-                <Header style={{alignItems: 'center'}}>
-                    <Body>
+                <Header style={{ flexDirection: "column" }}>
+                    <Body style={{ paddingTop: 15 }}>
                         <Title>Login</Title>
                     </Body>
                 </Header>
-                <Content>
-                    <Card>
-                        <CardItem header>
-                            <Text>NativeBase</Text>
-                        </CardItem>
-                        <CardItem>
-                            <Body>
-                                <Text>
-                                //Your text here
-                                </Text>
-                            </Body>
-                        </CardItem>
-                        <CardItem footer>
-                            <Text>GeekyAnts</Text>
-                        </CardItem>
+                <Content style={{ paddingTop: 200, paddingRight:10, paddingLeft: 10 }}>
+                    <Card style={{ alignItems: 'center', paddingTop: 20 }}>
+                        <Item floatingLabel rounded>
+                            <Label>  Username</Label>
+                            <Input />
+                        </Item>
+                        <Item floatingLabel rounded>
+                            <Label>  Password</Label>
+                            <Input />
+                        </Item>
+                        <Item>
+                            <Button rounded style={{ margin: 20 }}>
+                                <Text>GO!</Text>
+                            </Button>
+                        </Item>
                     </Card>
                 </Content>
             </Container>
