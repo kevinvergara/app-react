7aad12a3a7bd957518ebd7c3fd54b89d87386181
kevin vergara <kevinvergara@workmate.cl>
2019-12-07 01:15:51 -0300

    prueba la wea bastardo

--- app/boot/index.js
+++ app/boot/index.js
@@ -2,17 +2,18 @@ import React, { Component, Fragment } from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
 import { Images } from "../../assets/index";
 
-export default class Container extends Component {
+export default class Container extends Component<{}, State> {
     render() {
         return (
             <Fragment>
-                <View style={styles.container}>
-                    <View style={{flex: 1, flexDirection: "row", borderColor: "red", borderWidth: 2}}>
-                        <Image style={{borderWidth: 2, borderColor: 'blue', width: 60, height: 60}} uri={Images.IconApp}/>
-                        <Text>AFAFLM L SMLSMF LAS</Text>
-                    </View>
-                    <Text> Login to your Accsssount</Text>
+                <View style={{flex: 5, flexDirection: "column", borderColor: "red", borderWidth: 2}}>
+                    <Text>AFAFLM L SMLSMF LAS</Text>
+                    <Image 
+                    style={{borderWidth: 2, borderColor: 'blue', width: 600, height: 400}} 
+                    uri={'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi8yu2A1aLmAhU1E7kGHXhTDZMQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.agiliacenter.com%2Freactjs-vs-react-native%2F&psig=AOvVaw2p5pZ8ODpKmOF2z0IbB8LT&ust=1575778077498604'}
+                    />
                 </View>
+                <Text> Login to your Accsssount</Text>
             </Fragment>
         )
     };
@@ -20,8 +21,5 @@ export default class Container extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        flexDirection: "column"
     },
 });
